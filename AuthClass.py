f4fed749b3f2e53fb78a4f4253562978816e5b67
Dmitriy Korotov <gugameiru@gmail.com>
2019-05-15 00:29:30 +0300

    __init__ method added and tested.

--- AuthClass.py
+++ AuthClass.py
@@ -5,13 +5,15 @@ import os
 
 class auth:
 
-    def __init__(self):
-        pass
-
+    
     def __init__(self):
         self.names = {}
         self.dataname = 'db_names.txt'
         self.myfile = open(self.dataname, mode='r')
-        json_data = json.load(self.myfile)
+        try:
+            json_data = json.load(self.myfile)
+        except json.decoder.JSONDecodeError as err:
+            json_data = {}
         self.names = json_data
         self.myfile.close()
+
--- mytests.py
+++ mytests.py
@@ -17,3 +17,19 @@ class AuthClassTest(unittest.TestCase):
         self.myfile.close()
         self.user1 = auth()
         self.assertEqual(self.names, self.user1.names)
+
+    def test_empty_file(self):
+        self.names = {}
+        self.dataname = 'db_names.txt'
+        self.myfile = open('db_names.txt', 'tw', encoding='utf-8')
+        self.myfile.close()
+        self.myfile = open(self.dataname, mode='r')
+        try:
+            json_data = json.load(self.myfile)
+        except json.decoder.JSONDecodeError as err:
+            json_data = {}
+        self.names = json_data
+        self.myfile.close()
+        self.user2 = auth()
+        self.assertEqual(self.names, self.user2.names)
+

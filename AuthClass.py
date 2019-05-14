46eeeaca114713ef9e38436d802d12aa59ca2ae0
Dmitriy Korotov <gugameiru@gmail.com>
2019-05-15 00:21:54 +0300

    method added

--- AuthClass.py
+++ AuthClass.py
@@ -7,3 +7,11 @@ class auth:
 
     def __init__(self):
         pass
+
+    def __init__(self):
+        self.names = {}
+        self.dataname = 'db_names.txt'
+        self.myfile = open(self.dataname, mode='r')
+        json_data = json.load(self.myfile)
+        self.names = json_data
+        self.myfile.close()

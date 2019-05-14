c47c5777efbe2837731034bfbe3c34e056449df3
Dmitriy Korotov <gugameiru@gmail.com>
2019-05-15 00:33:31 +0300

    test_is_existing_file added

--- mytests.py
+++ mytests.py
@@ -33,3 +33,12 @@ class AuthClassTest(unittest.TestCase):
         self.user2 = auth()
         self.assertEqual(self.names, self.user2.names)
 
+    def test_is_using_existing_file(self):
+        
+        self.dataname = 'db_names_2.txt'
+        self.myfile = open('db_names_2.txt', 'tw', encoding='utf-8')
+        self.myfile.close()
+        self.user3 = auth()
+        self.user3.create_db(self.dataname,1)
+        self.assertEqual(self.dataname, self.user3.dataname)
+
